667cc3aada7ae7e59b48ece4f6f8839f51dc3555
Sowmya <sowmya.snhu@gmail.com>
2018-05-18 06:38:43 -0400

    making custom

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,6 @@
         Welcome to our world.
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
